b312e339c3e6068b1202cb38cb6a6448b8502154
andrey <pyankoff@gmail.com>
2019-02-11 00:05:40 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+import Constants from './Constants'
+import logo from './static/icon_transparent_512.png'
+import itunes from './static/app_store.svg'
+import gplay from './static/google_play.png'
+
+const Link = props => (
+  <Text
+    {...props}
+    accessibilityRole="link"
+    style={StyleSheet.compose(
+        styles.link,
+        props.style
+    )}
+  />
+);
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.app}>
+          <View style={{maxWidth: 740, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap'}}>
+            <Image
+              source={logo}
+              style={{width: 240, height: 240,}}
+            />
+
+            <View style={{flex: 1, minWidth: 300, alignItems: 'flex-start', paddingHorizontal: 10, }}>
+              <Text
+                accessibilityRole="heading"
+                style={{fontSize: 72, fontWeight: '200', }}>
+                Campfire
+              </Text>
+              <Text style={{fontSize: 20, lineHeight: 32, fontWeight: '400', marginVertical: 15, marginLeft: 2,}}>
+                Discover interesting topics and join the conversation
+              </Text>
+              <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 10,}}>
+                <Image
+                  accessibilityRole="link"
+                  href='https://itunes.apple.com/app/id1451465193'
+                  source={itunes}
+                  style={{width: 120, height: 40}}
+                />
+                <Image
+                  accessibilityRole="link"
+                  href={'https://play.google.com/store/apps/details?id=app.campfire'}
+                  source={gplay}
+                  style={{width: 156, height: 50}}
+                />
+              </View>
+            </View>
+
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Constants.back,
+    flex: 1,
+  },
+  app: {
+    flex: 1,
+    marginHorizontal: 'auto',
+    backgroundColor: Constants.back,
+    maxWidth: 920,
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    color: Constants.text,
+    fontWeight: "bold",
+    fontSize: "2rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  text: {
+    color: Constants.text,
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
+});
+
+export default App;
---
+++ src/Constants.js
@@ -0,0 +1,30 @@
+export default {
+  card: '#fff',
+
+  back: '#15202a',
+  tab: '#1f303f',
+  red: '#ff5e5e',
+  green: '#1dd1a1',
+  yellow: '#f6e58d',
+  gold: '#f9ca24',
+
+  black: '#000',
+  darkestgray: '#0a0a0a',
+  darkergray: '#333',
+  darkgray: '#555',
+  gray: '#999',
+  inactive: '#808d9d',
+  lightgray: '#dfdfdf',
+  lightergray: '#f4f4f4',
+  white: '#fff',
+
+  text: '#f4f4f4',
+
+  tint: '#f4f4f4',
+  dark: '#273a7f',
+  accent: '#FFAA1E',
+  light: '#fad89d',
+
+  adsEnabled: false,
+  forceProd: true,
+};
